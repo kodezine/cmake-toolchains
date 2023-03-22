70145dd13b9074388e09c0c45a6d88d9681ed46f
Sohal <tosohal@gmail.com>
2023-03-22 16:58:21 +0100

    made cache strings

--- cortex/cm0.cmake
+++ cortex/cm0.cmake
@@ -1,7 +1,7 @@
 # Cortex M0 based flags for compilation and linking
-set(cpu_flag cortex-m0)
-set(ac6_target arm-arm-none-eabi)
+set(cpu_flag "cortex-m0" CACHE STRING)
+set(ac6_target "arm-arm-none-eabi" CACHE STRING)
 set(fpu_type )
 set(float_abi )
-set(cpu_link_flags Cortex-M0)
+set(cpu_link_flags "Cortex-M0" CACHE STRING)
 set(llvm_config_file "armv6m_soft_nofp_semihost.cfg")
--- cortex/cm7.cmake
+++ cortex/cm7.cmake
@@ -1,7 +1,7 @@
 # Cortex M7 based flags for compilation and linking
-set(cpu_flag cortex-m7)
-set(ac6_target arm-arm-none-eabi)
-set(fpu_type fpv5-d16)
-set(float_abi hard)
-set(cpu_link_flags Cortex-M7.fp.dp)
-set(llvm_config_file "armv7em_hard_fpv5_d16.cfg")
+set(cpu_flag "cortex-m7" CACHE STRING)
+set(ac6_target "arm-arm-none-eabi" CACHE STRING)
+set(fpu_type "fpv5-d16" CACHE STRING)
+set(float_abi "hard" CACHE STRING)
+set(cpu_link_flags "Cortex-M7.fp.dp" CACHE STRING)
+set(llvm_config_file "armv7em_hard_fpv5_d16.cfg" CACHE STRING)
