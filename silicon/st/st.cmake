85f099d00bbd7588afe336e59ced41d3300313d2
sohal <tosohal@gmail.com>
2023-05-25 10:50:03 +0200

    Update st.cmake

--- silicon/st/st.cmake
+++ silicon/st/st.cmake
@@ -1,8 +1,8 @@
 # overarching HAL and CMSIS driver repo fetching mechanism for STM32 microcontrollers
 
 if(${STM32_TYPE} STREQUAL "f0")
-    include(silicon/st/hal_${STM32_TYPE}/st_CMSIS.cmake)
-    include(silicon/st/hal_${STM32_TYPE}/st_HAL.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/hal_${STM32_TYPE}/st_CMSIS.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/hal_${STM32_TYPE}/st_HAL.cmake)
 else()
 message(FATAL_ERROR "STM Drivers can not be defined ")
 endif()
