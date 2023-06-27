8679f9e4c47529f15424827bb7a625cde1c0a97d
sohal <tosohal@gmail.com>
2023-06-27 10:02:23 +0200

    update flags for floating point

--- common/ac6.cmake
+++ common/ac6.cmake
@@ -46,7 +46,7 @@ endfunction(setTargetCompileOptions)
 function(setTargetLinkOptions PROJECTNAME)
     target_link_options( ${${PROJECTNAME}}
         PUBLIC
-        ${ac6_link_flag}
+        --cpu ${ac6_link_flag}
         --strict
         "$<$<CONFIG:Debug>:--bestdebug>"            # Debug symbols
         "$<$<CONFIG:Release>:--no_debug>"           # No Debug symbols
--- cortex/cm7.cmake
+++ cortex/cm7.cmake
@@ -2,7 +2,7 @@
 set(cpu_flag "-mcpu=cortex-m7" CACHE STRING "Cortex CPU")
 set(cpu_mode "-mthumb" CACHE STRING "ARM Mode")
 set(ac6_target "--target=arm-arm-none-eabi" CACHE STRING "AC6 Target")
-set(ac6_link_flag "--cpu Cortex-M7")
+set(ac6_link_flag "Cortex-M7.fp.dp")
 set(fpu_type "-mfpu=fpv5-d16" CACHE STRING "Floating Point Type")
 set(float_abi "-mfloat-abi=hard" CACHE STRING "ABI For floating point")
 set(llvm_config_file "--config armv7em_hard_fpv5_d16.cfg" CACHE STRING "LLVM configuration file")
