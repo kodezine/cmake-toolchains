f5c463131f0fc0289b75484a79cc4c624133d8a2
Sohal <tosohal@gmail.com>
2023-10-13 18:24:35 +0200

    export symbol for CMSIS V5 include path

--- frameworks/arm_cmsis/arm_cmsis.cmake
+++ frameworks/arm_cmsis/arm_cmsis.cmake
@@ -5,17 +5,18 @@ set(libName "cmsis")
 
 if ($ENV{USE_PRECOMPILED_LIBS})
     set(GITHUB_BRANCH_CMSIS "5.9.0")
-    set(GITHUB_BRANCH_CMSIS_SHA3_256 "57f244324c8557347cf0564fed901efc3f09c9def1b88849eed3e23bdb38b6b5")
-
-    FetchContent_Declare(
-        ${libName}                             # Recommendation: Stick close to the original name.
-        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-        URL https://github.com/kodezine/cmsis-v5/releases/download/v0.1.0/${libName}-${GITHUB_BRANCH_CMSIS}-$ENV{CORTEX_TYPE}-${CMAKE_C_COMPILER_ID}-${CMAKE_C_COMPILER_VERSION}.tar.gz
-        #URL_HASH SHA3_256=${GITHUB_BRANCH_CMSIS_SHA3_256}
-        FIND_PACKAGE_ARGS NAMES ${libName}
-    )
-    FetchContent_MakeAvailable(${libName})
-    message(STATUS "${libName}: Successfully extracted precompiled library from GitHub")
+    if (DEFINED GITHUB_PRECOMPILED_CMSIS_V5_TAGNAME)
+        FetchContent_Declare(
+            ${libName}                             # Recommendation: Stick close to the original name.
+            DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+            URL https://github.com/kodezine/cmsis-v5/releases/download/v${GITHUB_PRECOMPILED_CMSIS_V5_TAGNAME}/${libName}-${GITHUB_BRANCH_CMSIS}-$ENV{CORTEX_TYPE}-${CMAKE_C_COMPILER_ID}-${CMAKE_C_COMPILER_VERSION}.tar.gz
+            FIND_PACKAGE_ARGS NAMES ${libName}
+        )
+        FetchContent_MakeAvailable(${libName})
+        message(STATUS "${libName}: precompiled library version v${GITHUB_PRECOMPILED_CMSIS_V5_TAGNAME} successfully extracted")
+    else ()
+        message(FATAL_ERROR "${libName}: Valid tag not provided for precompiled library")
+    endif ()
 else ()
     set(GITHUB_BRANCH_CMSIS "5.9.0")
     set(GITHUB_BRANCH_CMSIS_MD5 "6b67968b5a3540156a4bd772d899339e")
@@ -37,4 +38,11 @@ else ()
         add_subdirectory(${${libName}_SOURCE_DIR})
         message(STATUS "${libName}: Successfully extracted sources for static library from GitHub")
     endif()
+endif()
+
+# Export this for all users of CMSIS
+if($ENV{USE_PRECOMPILED_LIBS} STREQUAL "true")
+    set(CMSIS_INCLUDE_PATH "${cmsis_SOURCE_DIR}/include/cmsis" CACHE PATH "Path for the CMSIS Includes")
+else()
+    set(CMSIS_INCLUDE_PATH "${cmsis_SOURCE_DIR}/CMSIS/Core/Include" CACHE PATH "Path for the CMSIS Includes")
 endif()
