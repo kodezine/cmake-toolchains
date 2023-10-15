380d54ec073c3c5d150203ad1aff12c29678edf2
sohal <tosohal@gmail.com>
2023-10-15 15:49:23 +0200

    new cmsis-v5 attached

--- silicon/st/STM32Cube.cmake
+++ silicon/st/STM32Cube.cmake
@@ -12,8 +12,6 @@ string (TOLOWER ${STM32_TYPE} LOWERCASE_STM32_TYPE)
 # This brings the HAL and CMSIS from ST GitHub repos
 # only sources are fetched, configuration should be done by the master script
 if ((${UPPERCASE_STM32_TYPE} STREQUAL "F0") OR (${UPPERCASE_STM32_TYPE} STREQUAL "H7"))
-    set(STM32CUBEF0_MD5HASH "b39a990ccad47256e0a99b2b630a65b0")
-    set(STM32CUBEH7_MD5HASH "7457fb89c6a251167c968d169245b354")
     set(STM32CubeXX STM32Cube${UPPERCASE_STM32_TYPE})
     set(GITHUB_BRANCH_${STM32CubeXX} "v1.11.1")
     set(GITHUB_BRANCH_${STM32CubeXX}_MD5 ${STM32CUBE${UPPERCASE_STM32_TYPE}_MD5HASH})
@@ -23,7 +21,6 @@ if ((${UPPERCASE_STM32_TYPE} STREQUAL "F0") OR (${UPPERCASE_STM32_TYPE} STREQUAL
         cubexx                             # Recommendation: Stick close to the original name.
         DOWNLOAD_EXTRACT_TIMESTAMP TRUE
         URL https://github.com/STMicroelectronics/${STM32CubeXX}/archive/refs/tags/${GITHUB_BRANCH_${STM32CubeXX}}.tar.gz
-        URL_HASH MD5=${GITHUB_BRANCH_${STM32CubeXX}_MD5}
     )
 
     FetchContent_GetProperties(cubexx)
@@ -33,7 +30,7 @@ if ((${UPPERCASE_STM32_TYPE} STREQUAL "F0") OR (${UPPERCASE_STM32_TYPE} STREQUAL
     endif()
     # use the device family to set a cache variable for ARM Cortex Mx family here
     set (ARMCMSIS_DEVICE ARM$ENV{CORTEX_TYPE} CACHE STRING "CMSIS Arm Cortex Device type to match folder" FORCE)
-    cmake_print_variables(ARMCMSIS_DEVICE)
+#    cmake_print_variables(ARMCMSIS_DEVICE)
 else()
     message(FATAL_ERROR "STM Drivers can not be defined ")
 endif()
